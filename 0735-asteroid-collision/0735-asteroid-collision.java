07674b023246bf889c8372a52da853873e80784b
Shivam Keshri <76737107+Shivam-1400@users.noreply.github.com>
2023-07-20 10:35:38 +0530

    Time: 6 ms (88.29%), Space: 44.3 MB (33.94%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -6,17 +6,17 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (stk.isEmpty() || asteroids[i] > 0) {
                 stk.push(asteroids[i]);
-            } else {
+            } 
+            else {
                 while (!stk.isEmpty() && stk.peek() > 0 && stk.peek() < Math.abs(asteroids[i])) {
                     stk.pop();
                 }
 
                 if (!stk.isEmpty() && stk.peek() == Math.abs(asteroids[i])) {
                     stk.pop();
-                } else {
-                    if (stk.isEmpty() || stk.peek() < 0) {
-                        stk.push(asteroids[i]);
-                    }
+                } 
+                else if (stk.isEmpty() || stk.peek() < 0) {
+                    stk.push(asteroids[i]);
                 } 
             }
         }
