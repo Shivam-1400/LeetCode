b2b20d443c006feac2c27e1d03ac405086ff928d
Shivam Keshri <76737107+Shivam-1400@users.noreply.github.com>
2023-08-20 13:20:42 +0530

    Time: 98 ms (13.56%), Space: 59.1 MB (75.60%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,23 +1,69 @@
+/*
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+           priority_queue<pair<int,int>>pq;
+      vector<int>ans;
+      for(int i=0;i<nums.size();i++){
+          pq.push({nums[i],i});
+          if(i>=(k-1)){
+              while(i - pq.top().second >= k){
+                  pq.pop();
+              }
+              ans.push_back(pq.top().first);
+          }
+      }  
+      return ans;
+
+
+    }
+};
+*/
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> result = new ArrayList<>();
-        Deque<Integer> deque = new LinkedList<>();
+        List<Integer> ans= new ArrayList<>();
+        PriorityQueue<Pair<Integer, Integer>> pq= new PriorityQueue<>((a,b)->b.getKey()-a.getKey());
         
-        for (int j = 0, i = 0; j < nums.length; j++) {
-            while (!deque.isEmpty() && deque.peekLast() < nums[j]) {
-                deque.pollLast();
-            }
-            deque.offerLast(nums[j]);
+        for(int i=0; i< nums.length; i++){
+            // int[] ax= new int[]{nums[i], i};
+            pq.add(new Pair<>(nums[i], i));
             
-            if (j - i + 1 == k) {
-                result.add(deque.peekFirst());
-                if (deque.peekFirst() == nums[i]) {
-                    deque.pollFirst();
+            if(i>=k-1){
+                while(i- pq.peek().getValue() >=k){
+                    pq.poll();
                 }
-                i++;
+                ans.add(pq.peek().getKey());
             }
         }
-        
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        // System.out.println(ans);
+        int sz= ans.size();
+        int[] ax= new int[sz];
+        for(int i=0; i< sz; i++){
+            ax[i]= ans.get(i);
+        }
+        return ax;
     }
+    
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+//         List<Integer> result = new ArrayList<>();
+//         Deque<Integer> deque = new LinkedList<>();
+        
+//         for (int j = 0, i = 0; j < nums.length; j++) {
+//             while (!deque.isEmpty() && deque.peekLast() < nums[j]) {
+//                 deque.pollLast();
+//             }
+//             deque.offerLast(nums[j]);
+            
+//             if (j - i + 1 == k) {
+//                 result.add(deque.peekFirst());
+//                 if (deque.peekFirst() == nums[i]) {
+//                     deque.pollFirst();
+//                 }
+//                 i++;
+//             }
+//         }
+        
+//         return result.stream().mapToInt(Integer::intValue).toArray();
+//     }
 }
+
