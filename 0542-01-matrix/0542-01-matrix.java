f6e9877455926ec6eb962b8a83801eed2591e3a2
Shivam Keshri <76737107+Shivam-1400@users.noreply.github.com>
2023-08-20 12:46:59 +0530

    Time: 14 ms (55.62%), Space: 44.8 MB (99.40%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,42 +1,93 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        Queue<int[]> q = new LinkedList<>();
-        int rows = mat.length;
-        int cols = mat[0].length;
+        int m= mat.length;
+        int n= mat[0].length;
         
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (mat[i][j] == 0) {
-                    q.offer(new int[]{i, j});
-                } else {
-                    mat[i][j] = -1;
+        Queue<List<Integer>> qe= new LinkedList<>();
+        for(int i=0; i< m; i++){
+            for(int j=0; j< n; j++){
+                if(mat[i][j]==0){
+                    qe.add(List.of(i, j));
+                }
+                else{
+                    mat[i][j]= -1;
                 }
             }
         }
-        
-        int[] dirX = {1, -1, 0, 0};
-        int[] dirY = {0, 0, 1, -1};
-        
-        int length = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            length++;
-            while (size-- > 0) {
-                int[] front = q.poll();
-                int r = front[0];
-                int c = front[1];
-                for (int i = 0; i < 4; i++) {
-                    int newRow = r + dirX[i];
-                    int newCol = c + dirY[i];
-                    if (newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols || mat[newRow][newCol] >= 0) {
-                        continue;
+        int l=0; 
+        int nx[]={1, 0, -1, 0};
+        int ny[]={0, 1, 0, -1};
+        while(!qe.isEmpty()){
+            int sz= qe.size();
+            l++;
+            
+            for(int s=0; s< sz; s++){
+                List<Integer> ax= qe.remove();
+                int px= ax.get(0);
+                int py= ax.get(1);
+                
+                for(int k=0; k<4; k++){
+                    int newX= px+ nx[k];
+                    int newY= py+ ny[k];
+                    if(isValid(newX, newY, m, n, mat)){
+                        qe.add(List.of(newX, newY));
+                        if(mat[newX][newY] != 0){
+                            mat[newX][newY]= l;
+                        }
                     }
-                    mat[newRow][newCol] = length;
-                    q.offer(new int[]{newRow, newCol});
                 }
             }
+            
         }
-        
         return mat;
     }
+    boolean isValid(int x, int y, int m, int n, int [][]gr){
+        if(x>=0 && x< m && y>=0 && y< n){
+            if(gr[x][y] <0){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+//     public int[][] updateMatrix(int[][] mat) {
+//         Queue<int[]> q = new LinkedList<>();
+//         int rows = mat.length;
+//         int cols = mat[0].length;
+        
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 if (mat[i][j] == 0) {
+//                     q.offer(new int[]{i, j});
+//                 } else {
+//                     mat[i][j] = -1;
+//                 }
+//             }
+//         }
+        
+//         int[] dirX = {1, -1, 0, 0};
+//         int[] dirY = {0, 0, 1, -1};
+        
+//         int length = 0;
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+//             length++;
+//             while (size-- > 0) {
+//                 int[] front = q.poll();
+//                 int r = front[0];
+//                 int c = front[1];
+//                 for (int i = 0; i < 4; i++) {
+//                     int newRow = r + dirX[i];
+//                     int newCol = c + dirY[i];
+//                     if (newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols || mat[newRow][newCol] >= 0) {
+//                         continue;
+//                     }
+//                     mat[newRow][newCol] = length;
+//                     q.offer(new int[]{newRow, newCol});
+//                 }
+//             }
+//         }
+        
+//         return mat;
+//     }
 }
