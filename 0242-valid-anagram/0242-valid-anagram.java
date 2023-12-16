398f742429c9a642368ed14057531982a492628e
Shivam Keshri <76737107+Shivam-1400@users.noreply.github.com>
2023-12-16 21:00:34 +0530

    Time: 4 ms (73.04%), Space: 44.3 MB (6.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,31 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int n= s.length();
-        int m= t.length();
-        if(n != m){
+        if(s.length() != t.length()){
             return false;
         }
-        Map<Character, Integer> smp= new HashMap<>();
-        Map<Character, Integer> tmp= new HashMap<>();
-        for(int i=0; i< n; i++){
-            char sx= s.charAt(i);
-            smp.put(sx, smp.getOrDefault(sx, 0)+1);
-            
-            char tx= t.charAt(i);
-            tmp.put(tx, tmp.getOrDefault(tx, 0)+1);
-        }
-        int fla=0;
-        for(Map.Entry<Character, Integer> et: smp.entrySet()){
-            if(tmp.containsKey(et.getKey())){
-                if(tmp.get(et.getKey()).equals(et.getValue())){
-                    continue;
-                }
-                fla= 1;
-                break;
+        char[] sz= s.toCharArray();
+        char tz[]= t.toCharArray();
+        
+        Arrays.sort(sz);
+        Arrays.sort(tz);
+        
+        for(int i=0; i< sz.length; i++){
+            if(sz[i] != tz[i]){
+                return false;
             }
-            fla= 1;
-            break;
         }
-        return fla==0;
+        return true;
     }
 }
