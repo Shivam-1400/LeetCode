fdfb627b777e23dfdf95b77c4584c213b0d64404
Shivam Keshri <76737107+Shivam-1400@users.noreply.github.com>
2024-01-10 03:27:59 +0530

    Time: 7 ms (52.01%), Space: 44.5 MB (42.59%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,38 +7,68 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> ls1= new ArrayList<>();
-        getPath(root, p.val, ls1);
+        return solve(root, p.val, q.val);
         
-        List<TreeNode> ls2= new ArrayList<>();
-        getPath(root, q.val, ls2);
-        
-        ls1.retainAll(ls2);
-        return ls1.get(ls1.size()-1);
-        
-    }   
-    int getPath(TreeNode rt, int tr, List<TreeNode> ls){
+    }
+    TreeNode solve(TreeNode rt, int p, int q){
         if(rt== null){
-            return -1;
+            return null;
         }
         
-        if(rt.val== tr){
-            ls.add(rt);
-            return 1;
+        if(rt.val== p){
+            return rt;
         }
+        if(rt.val== q){
+            return rt;
+        }
+        TreeNode lft= solve(rt.left, p, q);
+        TreeNode rgt= solve(rt.right, p, q);
         
-        int a=  getPath(rt.left, tr, ls);
-        if(a==1){
-            ls.addFirst(rt);
-            return 1;
+        if(lft!= null && rgt != null){
+            return rt;
         }
-        int b=  getPath(rt.right,tr, ls);
-        if(b==1){
-            ls.addFirst(rt);
-            return 1;
+        if(lft != null){
+            return lft;
         }
-        return -1;
+        return rgt;
     }
 }
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         List<TreeNode> ls1= new ArrayList<>();
+//         getPath(root, p.val, ls1);
+        
+//         List<TreeNode> ls2= new ArrayList<>();
+//         getPath(root, q.val, ls2);
+        
+//         ls1.retainAll(ls2);
+//         return ls1.get(ls1.size()-1);
+        
+//     }   
+//     int getPath(TreeNode rt, int tr, List<TreeNode> ls){
+//         if(rt== null){
+//             return -1;
+//         }
+        
+//         if(rt.val== tr){
+//             ls.add(rt);
+//             return 1;
+//         }
+        
+//         int a=  getPath(rt.left, tr, ls);
+//         if(a==1){
+//             ls.addFirst(rt);
+//             return 1;
+//         }
+//         int b=  getPath(rt.right,tr, ls);
+//         if(b==1){
+//             ls.addFirst(rt);
+//             return 1;
+//         }
+//         return -1;
+//     }
+// }
