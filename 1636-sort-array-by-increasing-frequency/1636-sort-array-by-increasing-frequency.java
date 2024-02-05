f22e2fa327463d4a27d9bed367564376cd46cdce
Shivam Keshri <76737107+Shivam-1400@users.noreply.github.com>
2024-02-06 01:40:46 +0530

    Time: 13 ms (13.72%), Space: 44.7 MB (13.53%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -8,15 +8,20 @@ class Solution {
         List<Integer> ls= new ArrayList<>(mp.keySet());
         System.out.println(ls);
         // Collections.sort(ls, (a,b)->mp.get(a)-mp.get(b));
-        // Collections.sort(ls, (a,b)->mp.get(a)-mp.get(b) );
-        Collections.sort(ls,new Comparator<Integer>(){
-            public int compare(Integer a,Integer b){
-                if(mp.get(a) == mp.get(b)){
-                    return b-a;
-                }
-                return mp.get(a) - mp.get(b);
-            } 
-        });
+        Collections.sort(ls, (a,b)->{
+            if(mp.get(a)== mp.get(b))return b-a;
+            else return mp.get(a)-mp.get(b);
+            });
+        
+        
+        // Collections.sort(ls,new Comparator<Integer>(){
+        //     public int compare(Integer a,Integer b){
+        //         if(mp.get(a) == mp.get(b)){
+        //             return b-a;
+        //         }
+        //         return mp.get(a) - mp.get(b);
+        //     } 
+        // });
         
         System.out.println(ls);
         int j=0;
