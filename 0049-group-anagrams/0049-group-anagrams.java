243eaeb570f64d8636baedee9018aaa77c51b15f
Shivam Keshri <76737107+Shivam-1400@users.noreply.github.com>
2024-02-06 16:49:15 +0530

    Time: 13 ms (27.15%), Space: 48.3 MB (14.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,28 +1,87 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ans= new ArrayList<>();
-        HashMap<String, ArrayList<String>> map= new HashMap<>();
-        for(String s: strs){
-            char[] ch= s.toCharArray();
+        
+        Map<String, List<String>> mp= new HashMap<>();
+        
+        for(String x: strs){
+            char[] chArr= x.toCharArray();
+            Arrays.sort(chArr);
+            String temp= new String(chArr);
             
-            Arrays.sort(ch);
-            String sortedStr= new String(ch);
-            
-            if(map.containsKey(sortedStr)){
-                ArrayList<String> ls= map.get(sortedStr);
-                ls.add(s);
-                map.put(sortedStr, ls);
+            if(mp.containsKey(temp)){
+                List<String> ls= mp.get(temp);
+                ls.add(x);
+                mp.put(temp, ls );
             }
             else{
-                ArrayList<String> ls= new ArrayList<>();
-                ls.add(s);
-                map.put(sortedStr, ls);
+                List<String> ls= new ArrayList<>();
+                ls.add(x);
+                mp.put(temp, ls);
             }
         }
-        
-        for(String x: map.keySet()){
-            ans.add(new ArrayList<>(map.get(x)));
+        System.out.println(mp);
+        for(List<String> l: mp.values()){
+            ans.add(l);
         }
+        
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         List<List<String>> ans= new ArrayList<>();
+//         HashMap<String, ArrayList<String>> map= new HashMap<>();
+//         for(String s: strs){
+//             char[] ch= s.toCharArray();
+            
+//             Arrays.sort(ch);
+//             String sortedStr= new String(ch);
+            
+//             if(map.containsKey(sortedStr)){
+//                 ArrayList<String> ls= map.get(sortedStr);
+//                 ls.add(s);
+//                 map.put(sortedStr, ls);
+//             }
+//             else{
+//                 ArrayList<String> ls= new ArrayList<>();
+//                 ls.add(s);
+//                 map.put(sortedStr, ls);
+//             }
+//         }
+        
+//         for(String x: map.keySet()){
+//             ans.add(new ArrayList<>(map.get(x)));
+//         }
+//         return ans;
+//     }
+// }
